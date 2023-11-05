5fab29d23fe2e98c9f4aa2207292bb3437887930
andrea <33090191+cipollan@users.noreply.github.com>
2023-11-05 16:13:26 +0100

    Add files via upload

---
+++ MyArtifactIdSpringJpa/src/test/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplicationTests.java
@@ -0,0 +1,42 @@
+package com.bezkoder.spring.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+ 
+
+import com.mymagic.SpringBootMyMagicApplication;
+
+@SpringBootTest
+@SpringBootConfiguration
+@ContextConfiguration
+class SpringBootDataJpaApplicationTests {
+
+	
+  private static  Logger log = LoggerFactory.getLogger(SpringBootDataJpaApplicationTests.class);
+	
+	 
+  @Test
+  void contextLoads() {
+		
+		log.info("+-------------- <SpringBootDataJpaApplicationTests.contextLoads> -----------------------+" );
+	}
+  
+  @Test
+  void contextLoads2() {
+		
+		log.info("+-------------- <SpringBootDataJpaApplicationTests.contextLoads2> -----------------------+" );
+  }
+	
+  SpringBootDataJpaApplicationTests() 
+	{
+		
+		System.out.print("SpringBootDataJpaApplicationTests ");
+		
+	}
+	
+}
